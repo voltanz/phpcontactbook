8cc5e2c2dcb93ebcc3745094ad9710da092dcf6b
Yoppy Yunhasnawa <yunhasnawa@ymail.com>
2024-02-19 00:50:01 +0700

    initial commit

---
+++ data.sql
@@ -0,0 +1,12 @@
+-- Run the following SQL commands to create the database in your server
+
+CREATE DATABASE IF NOT EXISTS contacts_db;
+
+USE contacts_db;
+
+CREATE TABLE IF NOT EXISTS contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL
+);
